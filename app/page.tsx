6af4ef43c34f24ffe587d19398d19ee6133849b2
carlosmontealto <carloseduardo.montealto@gmail.com>
2024-05-10 14:58:15 -0300

    feat: add restaurant list component

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import ProductList from "./_components/product.list";
 import PromoBanner from "./_components/promo-banner";
+import RestaurantList from "./_components/restaurant-list";
 import Search from "./_components/search";
 import { Button } from "./_components/ui/button";
 import { db } from "./_lib/prisma";
@@ -30,7 +31,7 @@ const Home = async () => {
         <Search />
       </div>
 
-      <div className="px-5 pt-6">
+      <div className=" pt-6">
         <CategoryList />
       </div>
 
@@ -60,6 +61,20 @@ const Home = async () => {
           src="/promo-banner-02.png"
           alt="A partir de R$17,90 em lanches"
         />
+      </div>
+
+      <div className="space-y-4 py-6 pt-6">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Restaurantes Recomendados</h2>
+          <Button
+            variant="ghost"
+            className="h-fit p-0 text-primary hover:bg-transparent"
+          >
+            Ver todos
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+        <RestaurantList />
       </div>
     </>
   );
