acabfa4eccf2b80c8bd47d87f0c3bcf624ff9e9c
Carlos Montealto <kadu_1401@hotmail.com>
2024-05-27 15:36:50 -0300

    feat: add recommended product page

---
+++ app/products/recommended/page.tsx
@@ -0,0 +1,38 @@
+import Header from "@/app/_components/header";
+import ProductItem from "@/app/_components/product-item";
+import { db } from "@/app/_lib/prisma";
+
+const RecommendedProductsPage = async () => {
+  const products = await db.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+    take: 10,
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+  return (
+    <>
+      <Header />
+      <h2 className="px-5 py-6 text-lg font-semibold">Pedidos Recomendados</h2>
+      <div className="grid w-full grid-cols-2 gap-6 px-5">
+        {products.map((product) => (
+          <ProductItem
+            key={product.id}
+            product={product}
+            className="w-full min-w-full"
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default RecommendedProductsPage;
