d8e3e67587bb10e022cf7b1df90b581fe0291874
Carlos Montealto <kadu_1401@hotmail.com>
2024-07-02 10:17:13 -0300

    feat: add favorite restaurants page

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -92,9 +92,12 @@ const Header = () => {
             <Button
               variant="ghost"
               className="w-full justify-start space-x-3 rounded-full text-sm font-normal"
+              asChild
             >
-              <HomeIcon size={16} />
-              <span className="block">Inicio</span>
+              <Link href="/">
+                <HomeIcon size={16} />
+                <span className="block">Inicio</span>
+              </Link>
             </Button>
             {data?.user && (
               <>
@@ -111,9 +114,12 @@ const Header = () => {
                 <Button
                   variant="ghost"
                   className="w-full justify-start space-x-3 rounded-full text-sm font-normal"
+                  asChild
                 >
-                  <HeartIcon size={16} />
-                  <span className="block">Restaurantes Favoritos</span>
+                  <Link href="/my-favorite-restaurants">
+                    <HeartIcon size={16} />
+                    <span className="block">Restaurantes Favoritos</span>
+                  </Link>
                 </Button>
               </>
             )}
---
+++ app/my-favorite-restaurants/page.tsx
@@ -0,0 +1,51 @@
+import { getServerSession } from "next-auth";
+import { notFound } from "next/navigation";
+
+import { authOptions } from "../_lib/auth";
+import { db } from "../_lib/prisma";
+
+import Header from "../_components/header";
+import RestaurantItem from "../_components/restaurant-item";
+
+const MyFavoriteRestaurants = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return notFound();
+  }
+
+  const userFavoriteRestaurants = await db.userFavoriteRestaurant.findMany({
+    where: {
+      userId: session.user.id,
+    },
+    include: {
+      restaurant: true,
+    },
+  });
+  return (
+    <>
+      <Header />
+      <h2 className="px-5 py-6 text-lg font-semibold">
+        Restaurantes Favoritos
+      </h2>
+      <div className="flex w-full flex-col gap-6 px-5">
+        {userFavoriteRestaurants.length > 0 ? (
+          userFavoriteRestaurants.map(({ restaurant }) => (
+            <RestaurantItem
+              key={restaurant.id}
+              restaurant={restaurant}
+              className="min-w-full max-w-full"
+              userFavoritesRestaurants={userFavoriteRestaurants}
+            />
+          ))
+        ) : (
+          <h3 className="font-medium">
+            Você ainda não adicionou nenhum restaurante aos seus favoritos!
+          </h3>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default MyFavoriteRestaurants;
